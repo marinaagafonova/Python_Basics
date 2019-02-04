98cd71524f95ab98e0402dc1356dfa18148a91e4
Marina Agafonova <42440523+marinaagafonova@users.noreply.github.com>
2019-02-04 20:15:28 +0300

    Create 2_5 Lists (string split).py

---
+++ 2_5 Lists (string split).py
@@ -0,0 +1,7 @@
+#Напишите программу, на вход которой подается одна строка с целыми числами. Программа должна вывести сумму этих чисел.
+
+numbers = (int(i) for i in input().split());
+result = 0;
+for i in numbers:
+    result += i;
+print(result);

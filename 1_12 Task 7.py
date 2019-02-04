d9c278a1e7e432b000ec8232d3cf904258de45af
Marina Agafonova <42440523+marinaagafonova@users.noreply.github.com>
2019-02-04 15:27:20 +0300

    Update 1_12 Task 7.py

--- 1_12 Task 7.py
+++ 1_12 Task 7.py
@@ -1,4 +1,4 @@
-Счастливый билет или нет.
+#Счастливый билет или нет.
 
 number = int(input());
 if (number//1e5 + (number//1e4)%10 + (number//1e3)%10) == (number%10 + (number%100)//10 + (number%1000)//100):
